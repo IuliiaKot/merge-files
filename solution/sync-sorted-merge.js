00673227ce48eed8ce08d05ab743a1dc06384fc9
IuliiaKot <kotlenko.julia@gmail.com>
2017-09-15 12:00:34 -0400

    add part1

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -4,5 +4,37 @@ const BinaryHeap = require('./minHeap');
 const MinHeapNode = require('./heapNode');
 
 module.exports = (logSources, printer) => {
-	// throw new Error('Not implemented yet!  That part is up to you!')
+	let minHeap = new BinaryHeap();
+
+// take first object from log files and add to heap
+// after insert min value will be on top of heap
+	for(let i = 0; i < logSources.length; i++){
+		let tmp = logSources[i].pop();
+		let node = new MinHeapNode({
+			value: tmp.date,
+			idx: i,
+			msg: tmp.msg
+		});
+		minHeap.insert(node);
+	}
+
+// continue do this while heap is not empty. remove mean element
+// from heap and take next elemnt from the same log file.
+
+	while (minHeap.heap.length > 0) {
+		let node = minHeap.removeMin();
+		printer.print({date: node.value, msg: node.msg});
+		let tmp = logSources[node.idx].pop();
+
+		if (tmp) {
+			node = new MinHeapNode({
+				value: tmp.date,
+				idx: node.idx,
+				msg: tmp.msg
+			});
+			minHeap.insert(node);
+		}
+	}
+	printer.done();
+
 }
